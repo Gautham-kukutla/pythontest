820da330bc2fc3fb3a3425cebdff263dda7d75a6
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-09 20:03:29 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,9 @@ find mar2 -name *.py > file.txt
  while read -r line
  do autopep8 --in-place --aggressive --aggressive "$line" 
  done < file2
+ git add .
+ git diff --staged > file2.txt
+ 
   
 
  
