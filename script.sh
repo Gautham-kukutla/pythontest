19af2a96e876b4184b85c5d6a1cbd73c961e1d56
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-09 20:15:57 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,8 +7,9 @@ find mar2 -name *.py > file.txt
  done < file2
  git add .
  git diff --staged > file2.txt
+ if [ grep -c ".py$" 1.py -gt 0 ]
+ then git commit -am "Commit done"
+      git push "https://Gautham-kukutla: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@github.com/Gautham-kukutla/pythontest.git"
+ else echo "no changegs done"
+ fi
  
-  
-
- 
-
