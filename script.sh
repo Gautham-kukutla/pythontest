e78dab6911e6b55081bcbd1313daa644c305e6b8
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-09 20:19:44 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@ find mar2 -name *.py > file.txt
 
  while read -r line
  do autopep8 --in-place --aggressive --aggressive "$line" 
- done < file2
+ done < file.txt
  git add .
  git diff --staged > file2.txt
- if [ grep -c ".py$" 1.py -gt 0 ]
+ if [ grep -c ".py$" file2.txt -gt 0 ]
  then git commit -am "Commit done"
       git push "https://Gautham-kukutla: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@github.com/Gautham-kukutla/pythontest.git"
  else echo "no changegs done"
