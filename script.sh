28f11a283762fa8e46d52da24fcec1d18bbd6c1a
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-09 22:53:49 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,17 @@
 #!bin/bash
 #apt-get install python-autopep8
-find mar2 -name *.py > file.txt
+find mar2 -name '*.py' > file.txt
 
  while read -r line
  do autopep8 --in-place --aggressive --aggressive "$line" 
  done < file.txt
  git add .
  git status | grep .py$ > file.txt
- if [ -s file2.txt ]
+ if [ -s file.txt ]
  then echo "no changes done" 
- else  git commit -am "Commit done"
+ else rm file.txt
+     git add .
+     git commit -am "Commit done"
      git push "https://Gautham-kukutla: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@github.com/Gautham-kukutla/pythontest.git" 
  fi
  
